2a032f24292097a5f2f835ab532122598daeca07
Shiraz Muhammed <shirazmuhammed04@gmail.com>
2021-12-24 16:03:03 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// This is the main index.js file in the Eventogenic frontend project
+
+import react from "react";
+import reactDom from "react-dom";
+import App from "./App";
+
+reactDom.render(
+    <App />,
+    document.getElementById('root')
+)
