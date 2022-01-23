4704ea48ee441b118d6a6ce7c12d09776c71bc1b
Shiraz Muhammed <shirazmuhammed04@gmail.com>
2022-01-23 10:08:44 +0530

    scroll to top function deleted

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AppBarMain from "./components/AppBarMain";
 import { ThemeProvider } from "@mui/material/styles";
 import { Theme } from "./constants/Theme";
 import FloatingActionButton from "./components/asides/FloatingActionButton";
 
 const App = () => {
-  const [btnStatus, setBtnStatus] = useState(false);
-  useEffect(() => {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      setBtnStatus(true);
-    } else {
-      setBtnStatus(false);
-    }
-  });
+  
   return (
     <ThemeProvider theme={Theme}>
       <React.Fragment>
